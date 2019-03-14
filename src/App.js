08671965fc389db0729d2be7d28ce94a65217440
samli <samliprajapati9@gmail.com>
2019-03-14 12:53:53 +0530

    warning class added

--- src/App.js
+++ src/App.js
@@ -5,28 +5,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-  
       <div>
-        <html>
-          <head>
-            
-          </head>
-          <body>
         <h1>Login Here</h1>
-        <label for="uname"><b>Username : </b></label><br/>
-    <input type="text" placeholder="Enter Username" name="uname" required /><br/>
-    
-    <label for="psw"><b>Password : </b></label><br/>
-    <input type="password" placeholder="Enter Password" name="psw" required /><br/>
-        
-    <button className="btn btn-primary"type="submit">Login</button>&nbsp;
-    <button type="cancel">Cancel</button>
-    
-    </body>
-  </html>
+        <label for="uname"><b>Username : </b></label><br />
+        <input type="text" placeholder="Enter Username" name="uname" required /><br />
+
+        <label for="psw"><b>Password : </b></label><br />
+        <input type="password" placeholder="Enter Password" name="psw" required /><br />
+
+        <button className="btn btn-primary" type="submit">Login</button>&nbsp;
+    <button className="btn btn-warning" type="cancel">Cancel</button>
+
       </div>
-              );
-            }
-          }
-          
-          export default App;
+    );
+  }
+}
+
+export default App;
